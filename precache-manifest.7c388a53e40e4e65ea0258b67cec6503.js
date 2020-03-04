ceeb09ae9e3d2fd96ebb85503e844da3a785bb06
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-03-04 11:30:51 +0000

    Updates

---
+++ precache-manifest.7c388a53e40e4e65ea0258b67cec6503.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2b5870cb44a1bd086a97b9de8426c8",
+    "url": "/product-saas-rebuild/index.html"
+  },
+  {
+    "revision": "7e2cba94dad56eae7277",
+    "url": "/product-saas-rebuild/static/css/2.e4355e26.chunk.css"
+  },
+  {
+    "revision": "7a8fbbd80d6e8d249843",
+    "url": "/product-saas-rebuild/static/css/main.4826449c.chunk.css"
+  },
+  {
+    "revision": "7e2cba94dad56eae7277",
+    "url": "/product-saas-rebuild/static/js/2.0ec42f12.chunk.js"
+  },
+  {
+    "revision": "7a8fbbd80d6e8d249843",
+    "url": "/product-saas-rebuild/static/js/main.eeb4fc10.chunk.js"
+  },
+  {
+    "revision": "6fba3e5a02d1e1b4da4e",
+    "url": "/product-saas-rebuild/static/js/runtime-main.150f0b04.js"
+  },
+  {
+    "revision": "d089592c47c140e2318426aefb451b04",
+    "url": "/product-saas-rebuild/static/media/api.d089592c.yaml"
+  },
+  {
+    "revision": "ecc65d60f605c12d3fca79c517dc4195",
+    "url": "/product-saas-rebuild/static/media/download-icon-white.ecc65d60.svg"
+  },
+  {
+    "revision": "f5e25df7970ccd13967cf3ba2e766855",
+    "url": "/product-saas-rebuild/static/media/drop-grey.f5e25df7.svg"
+  },
+  {
+    "revision": "78cd02a100de1ab70ac31455baa11fa2",
+    "url": "/product-saas-rebuild/static/media/drop-white.78cd02a1.svg"
+  },
+  {
+    "revision": "34536746f7b8c5b46d8675f1941e1dbc",
+    "url": "/product-saas-rebuild/static/media/info-icon.34536746.svg"
+  },
+  {
+    "revision": "f2df34b5849355fed739e6a90915203a",
+    "url": "/product-saas-rebuild/static/media/logo.f2df34b5.svg"
+  },
+  {
+    "revision": "d41c631875217e983fe2051e2c62932e",
+    "url": "/product-saas-rebuild/static/media/modal-close.d41c6318.svg"
+  },
+  {
+    "revision": "b01fa7f07a4c2e355eff2be5069f4c93",
+    "url": "/product-saas-rebuild/static/media/montserrat-regular-webfont.b01fa7f0.woff2"
+  },
+  {
+    "revision": "e8b7d8acf640a69f838ca38c063e33ff",
+    "url": "/product-saas-rebuild/static/media/montserrat-regular-webfont.e8b7d8ac.woff"
+  },
+  {
+    "revision": "9d9e09d7ae09f64a8a365e669105bedd",
+    "url": "/product-saas-rebuild/static/media/roboto-regular-webfont.9d9e09d7.woff"
+  },
+  {
+    "revision": "d96d07c1e0f7cdf7a1988dcb0bf6e0e4",
+    "url": "/product-saas-rebuild/static/media/roboto-regular-webfont.d96d07c1.woff2"
+  }
+]);
